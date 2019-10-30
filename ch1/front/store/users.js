2742467b06227bf89cd3ff8b33c6a24c77864801
Sweep <jeonsh1991@gmail.com>
2019-10-30 23:57:40 +0900

    store users

--- ch1/front/store/users.js
+++ ch1/front/store/users.js
@@ -1,9 +1,23 @@
 export const state = () => ({
-    hello : "vuex",
-});
+    me: null,
+})
 
-export const mutations = () => ({
-    bye(state) {
-        state.hello =  "goodbye" ;
+// 동기처리
+export const mutations = {
+    setMe(state, payload) {
+        state.me = payload;
     }
-});
+}
+
+// 비동기처리    
+export const actions = {
+    signUp({ commit, state }, payload) {
+        commit("setMe", payload);
+    },
+    logIn({commit, state}, payload) {
+        commit("setMe", payload);
+    },
+    logOut({commit, state}, payload) {
+        commit("setMe", null);
+    },
+}
